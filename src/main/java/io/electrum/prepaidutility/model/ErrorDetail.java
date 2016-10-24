2f1dde299d58bd4f010be2195f228c1ee2c03ff7
neilmackellar <neilmackellar@electrum.co.za>
2016-10-24 16:36:26 +0200

    new fields

--- src/main/java/io/electrum/prepaidutility/model/ErrorDetail.java
+++ src/main/java/io/electrum/prepaidutility/model/ErrorDetail.java
@@ -47,8 +47,37 @@ public class ErrorDetail {
       }
    }
 
+   /**
+    * The following request types are recognised
+    */
+   public enum RequestType {
+      METER_LOOKUP_REQUEST("METER_LOOKUP_REQUEST"),
+      TOKEN_PURCHASE_REQUEST("TOKEN_PURCHASE_REQUEST"),
+      TOKEN_PURCHASE_RETRY_REQUEST("TOKEN_PURCHASE_RETRY_REQUEST"),
+      TOKEN_REPRINT_REQUEST("TOKEN_REPRINT_REQUEST"),
+      FAULT_REPORT_REQUEST("FAULT_REPORT_REQUEST"),
+      KEY_CHANGE_TOKEN_REQUEST("KEY_CHANGE_TOKEN_REQUEST"),
+      CONFIRMATION_ADVICE("CONFIRMATION_ADVICE"),
+      REVERSAL_ADVICE("REVERSAL_ADVICE");
+
+      private String value;
+
+      RequestType(String value) {
+         this.value = value;
+      }
+
+      @Override
+      @JsonValue
+      public String toString() {
+         return String.valueOf(value);
+      }
+   }
+
    private ErrorType errorType = null;
    private String errorMessage = null;
+   private RequestType requestType = null;
+   private String id = null;
+   private String originalId = null;
    private Object detailMessage = null;
 
    /**
@@ -91,6 +120,62 @@ public class ErrorDetail {
    }
 
    /**
+    * The type of request for which the error.
+    **/
+   public ErrorDetail requestType(RequestType requestType) {
+      this.requestType = requestType;
+      return this;
+   }
+
+   @ApiModelProperty(required = true, value = "The type of request being processed when the error occurred.")
+   @JsonProperty("requestType")
+   @NotNull
+   public RequestType getRequestType() {
+      return requestType;
+   }
+
+   public void setRequestType(RequestType requestType) {
+      this.requestType = requestType;
+   }
+
+   /**
+    * The UUID of the message for which error occurred.
+    **/
+   public ErrorDetail id(String id) {
+      this.id = id;
+      return this;
+   }
+
+   @ApiModelProperty(required = true, value = "The UUID of the message for which error occurred.")
+   @JsonProperty("id")
+   @NotNull
+   public String getId() {
+      return id;
+   }
+
+   public void setId(String id) {
+      this.id = id;
+   }
+
+   /**
+    * The UUID of the original request message in the case of an error occurring for an advice message.
+    **/
+   public ErrorDetail originalId(String originalId) {
+      this.originalId = originalId;
+      return this;
+   }
+
+   @ApiModelProperty(value = "The UUID of the original request message in the case of an error occurring for an advice message.")
+   @JsonProperty("originalId")
+   public String getOriginalId() {
+      return originalId;
+   }
+
+   public void setOriginalId(String originalId) {
+      this.originalId = originalId;
+   }
+
+   /**
     * A free form detailed description of a particular failure condition may optionally be supplied
     **/
    public ErrorDetail detailMessage(Object detailMessage) {
@@ -118,12 +203,14 @@ public class ErrorDetail {
       }
       ErrorDetail errorDetail = (ErrorDetail) o;
       return Objects.equals(errorType, errorDetail.errorType) && Objects.equals(errorMessage, errorDetail.errorMessage)
+            && Objects.equals(requestType, errorDetail.requestType) && Objects.equals(id, errorDetail.id)
+            && Objects.equals(originalId, errorDetail.originalId)
             && Objects.equals(detailMessage, errorDetail.detailMessage);
    }
 
    @Override
    public int hashCode() {
-      return Objects.hash(errorType, errorMessage, detailMessage);
+      return Objects.hash(errorType, errorMessage, requestType, id, originalId, detailMessage);
    }
 
    @Override
@@ -133,6 +220,9 @@ public class ErrorDetail {
 
       sb.append("    errorType: ").append(Utils.toIndentedString(errorType)).append("\n");
       sb.append("    responseMessage: ").append(Utils.toIndentedString(errorMessage)).append("\n");
+      sb.append("    requestType: ").append(Utils.toIndentedString(requestType)).append("\n");
+      sb.append("    id: ").append(Utils.toIndentedString(id)).append("\n");
+      sb.append("    originalId: ").append(Utils.toIndentedString(originalId)).append("\n");
       sb.append("    detailMessage: ").append(Utils.toIndentedString(detailMessage)).append("\n");
       sb.append("}");
       return sb.toString();
