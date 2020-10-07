28f292d3560bc3f2d876e19fd7f97163f57f5bdb
Clayton <sibandaclayton0@gmail.com>
2020-10-07 13:59:21 +0200

    added KeyChangeConfirmation and KeyChangeReversal

---
+++ src/main/java/io/electrum/prepaidutility/model/KeyChangeConfirmation.java
@@ -0,0 +1,22 @@
+package io.electrum.prepaidutility.model;
+
+import io.electrum.vas.model.BasicAdvice;
+import io.swagger.annotations.ApiModel;
+
+/**
+ * Represents an advice message confirming successful change of a key
+ */
+@ApiModel(description = "Represents an advice message confirming successful change of a key.")
+public class KeyChangeConfirmation extends BasicAdvice {
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("KeyChangeConfirmation{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", requestId='").append(requestId).append('\'');
+        sb.append(", time=").append(time);
+        sb.append(", thirdPartyIdentifiers=").append(thirdPartyIdentifiers);
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ src/main/java/io/electrum/prepaidutility/model/KeyChangeReversal.java
@@ -0,0 +1,22 @@
+package io.electrum.prepaidutility.model;
+
+import io.electrum.vas.model.BasicReversal;
+import io.swagger.annotations.ApiModel;
+
+/**
+ * Represents a request to reverse a previous key change request.
+ */
+@ApiModel(description = "Represents a request to reverse a previous key change request.")
+public class KeyChangeReversal extends BasicReversal {
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("KeyChangeReversal{");
+        sb.append("reversalReason=").append(reversalReason);
+        sb.append(", id='").append(id).append('\'');
+        sb.append(", requestId='").append(requestId).append('\'');
+        sb.append(", time=").append(time);
+        sb.append(", thirdPartyIdentifiers=").append(thirdPartyIdentifiers);
+        sb.append('}');
+        return sb.toString();
+    }
+}
